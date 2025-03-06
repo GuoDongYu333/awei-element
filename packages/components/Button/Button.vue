f5638addd930715f052938cd4303867792c1b423
GuoDongYu333 <738462315@qq.com>
2025-03-06 11:01:19 +0800

    feat(components): 实现 Button 组件并添加测试
    - 在 Button 组件中增加了各种属性和样式
    - 更新了 package.json 中的测试命令
    - 添加了组件测试配置
    - 更新了样式文件，增加了更多样式变量和规则

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,8 +1,38 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./type";
+
 defineOptions({
   name: "EzButton",
 });
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: "button",
+  nativeType: "button",
+});
+const slot = defineSlots();
+const _ref = ref<HTMLButtonElement>();
 </script>
 <template>
-  <button style="background-color: yellow; color: red">this is a button</button>
+  <component
+    :is="props.tag"
+    ref="_ref"
+    :type="tag === 'button' ? nativeType : void 0"
+    :disable="disabled || loading ? true : void 0"
+    class="ez-button"
+    :class="{
+      [`ez-button--${type}`]: type,
+      [`ez-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+  >
+    <slot></slot>
+  </component>
 </template>
+
+<style scoped>
+@import "./style.css";
+</style>
---
+++ packages/components/Button/type.ts
@@ -0,0 +1,27 @@
+import type { Component } from "vue";
+
+export type ButtonType =
+  | "primary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "info"
+  | "text";
+
+export type NativeType = "button" | "submit" | "reset";
+
+export type ButtonSize = "large" | "default" | "small";
+
+export interface ButtonProps {
+  tag?: string | Component;
+  type?: ButtonType;
+  size?: ButtonSize;
+  nativeType?: NativeType;
+  disabled?: boolean;
+  loading?: boolean;
+  icon?: string;
+  round?: boolean;
+  circle?: boolean;
+  plain?: boolean;
+  autoFocus?: boolean;
+}
